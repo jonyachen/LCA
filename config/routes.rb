26bb04c8fff8c02b22946e718c2da69060ed1acc
Ron <rqueensen@berkeley.edu>
2016-10-07 08:06:23 +0000

    heroku please

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => "page#index"
+  root "page#index"
 end
