343ab07f575ec7e0712a9a17348746e0b8c2d326
Sean Keegan <s.keegan@berkeley.edu>
2016-10-22 02:48:37 +0000

    added menu for +sub-assembly button

--- app/assets/javascripts/model.js
+++ app/assets/javascripts/model.js
@@ -8,6 +8,14 @@ function hide_select_panel() {
 	document.getElementById('select-panel').style.visibility = 'hidden';
 }
 
+function show_assembly_panel() {
+	document.getElementById('assembly-panel').style.visibility = 'visible';
+}
+
+function hide_assembly_panel() {
+	document.getElementById('assembly-panel').style.visibility = 'hidden';
+}
+
 $(document).ready( function() {
 	$('select').material_select();
 });
