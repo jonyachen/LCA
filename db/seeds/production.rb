f0e2cbf591aee9058359e3e3bed2e3645be28ef4
Ronan Pigott <rpigott@berkeley.edu>
2016-10-21 20:37:32 -0700

    added production seeds

---
+++ db/seeds/production.rb
@@ -0,0 +1,18 @@
+Material.delete_all
+Material.create!(:title => "Steel", :category => "Metal")
+Material.create!(:title => "Copper", :category => "Metal")
+Material.create!(:title => "Aluminum", :category => "Metal")
+Material.create!(:title => "Concrete", :category => "Ceramic")
+Material.create!(:title => "Glass", :category => "Ceramic")
+Material.create!(:title => "Clay", :category => "Ceramic")
+Material.create!(:title => "Wood", :category => "Wood")
+Material.create!(:title => "Acids", :category => "Chemicals")
+Material.create!(:title => "Epoxy", :category => "Polymers")
+
+Procedure.delete_all
+Procedure.create!(:title => "Rail", :category => "Transportation")
+Procedure.create!(:title => "Truck", :category => "Transportation")
+Procedure.create!(:title => "Boat", :category => "Transportation") 
+Procedure.create!(:title => "Hot Roll", :category => "Manufacturing", :material => "Steel") 
+Procedure.create!(:title => "Cold Roll", :category => "Manufacturing", :material => "Steel") 
+Procedure.create!(:title => "Tempering", :category => "Manufacturing", :material => "Glass") 
