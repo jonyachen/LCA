8ec6d0b0c4ce5583ed9eea598c1878fb0b1651e6
Ron <rqueensen@berkeley.edu>
2016-10-22 05:20:56 +0000

    code climate stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -5,7 +7,9 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'simplecov'
 
-SimpleCov.start
+SimpleCov.start do 
+  add_filter "coverage/assets/0.10.0/"
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
