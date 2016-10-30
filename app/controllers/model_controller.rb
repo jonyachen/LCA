3d7fb9199515ddeba59c8fdc6d20b09c87ff0514
Ronan Pigott <rpigott@berkeley.edu>
2016-10-30 14:00:15 -0700

    Added a spot for the material search

--- app/controllers/model_controller.rb
+++ app/controllers/model_controller.rb
@@ -2,6 +2,8 @@ class ModelController < ApplicationController
   def index
   	@categories = Material.categories
 
+  	@material_data = Hash[Material.all.collect {|material| [material.title, nil]}]
+
   	@material_options = Material.categories.collect do |category|
   		[category, Material.find_by_category(category).collect {|material| material.title}]
   	end
