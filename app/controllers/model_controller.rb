7f648ef1078586ba4db05fc49d4249e1858f78cb
Ronan Pigott <rpigott@berkeley.edu>
2016-10-30 12:28:27 -0700

    Added a library menu for the drag-drop-ui

--- app/controllers/model_controller.rb
+++ app/controllers/model_controller.rb
@@ -1,6 +1,10 @@
 class ModelController < ApplicationController
   def index
   	@categories = Material.categories
+
+  	@material_options = Material.categories.collect do |category|
+  		[category, Material.find_by_category(category).collect {|material| material.title}]
+  	end
     
     render 'index'
   end
