eb835bcea82cda9d301500efd3e8bd023801be1f
Daniel Mockaitis <daniel.mockaitis@berkeley.edu>
2016-11-06 17:32:27 -0800

    New styling to the UI

---
+++ features/step_definitions/homepage_steps.rb
@@ -0,0 +1,29 @@
+When(/^Given I am on the homepage$/) do
+  visit root_path
+end
+
+
+Given(/^the following Materials exist:$/) do |table|
+   table.hashes.each do |material|
+     Material.create(material)
+   end
+end
+
+Given(/^the following Processes exist:$/) do |table|
+   table.hashes.each do |process|
+     Procedure.create(process)
+   end
+end
+
+Then(/^I choose "([^"]*)" from "([^"]*)"$/) do |menu_option, menu|
+   within("td[id=#{menu}]") do
+      find('.collapsible-header', :text => menu_option).click
+   end
+
+end
+
+Then(/^I drag "([^"]*)" to "([^"]*)"$/) do |material, drop_area|
+   source = find('.draggable', :text => material)
+   target = find("td[id=#{drop_area}]")
+   source.drag_to(target)
+end
