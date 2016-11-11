21071264638889a498800bd1b05e97d10e87eba8
Daniel Mockaitis <daniel.mockaitis@berkeley.edu>
2016-11-11 00:27:03 -0800

    Login Features

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,13 @@
+
+Given(/^the following Users exist:$/) do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I am on the loginpage$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I am on the welcomepage$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
