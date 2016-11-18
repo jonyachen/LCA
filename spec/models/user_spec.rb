58234b490408496ef33bc3b1da58bcf66e147f55
Daniel Mockaitis <daniel.mockaitis@berkeley.edu>
2016-11-18 02:32:07 -0800

    Fixed Build Errors, Code Coverage Down

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,18 +4,21 @@ require 'factory_girl_rails'
 RSpec.describe User, type: :model do
   describe 'Create users' do
     it 'should create a User' do
-      User.new(:name => 'Daniel Mockaitis', :username => 'dm1234', :password => 'secretword', :email => 'dm@x.com').should be_valid
+      User.new(:name => 'Daniel Mockaitis', :username => 'dm1234', :password => 'secretword', :email => 'dm7@x.com').should be_valid
     end
+
     it 'should multiple user factory valid' do
-      User.new(:name => 'Daniel Mockaitis', :username => 'dm1234', :password => 'secretword', :email => 'dm@x.com').should be_valid
-      User.new(:name => 'Daniel2 Mockaitis2', :username => 'dm12345', :password => 'secretword2', :email => 'dm@x.com').should be_valid
+      User.new(:name => 'Daniel Mockaitis', :username => 'dm1234', :password => 'secretword', :email => 'dm3@x.com').should be_valid
+      User.new(:name => 'Daniel2 Mockaitis2', :username => 'dm12345', :password => 'secretword2', :email => 'dm2@x.com').should be_valid
     end
-    
+
   end
-  
+
   it 'Calling User.authenticate with a valid user/password combo returns a user' do
-      alice = User.create(:username => 'alice', :password => 'greatest', :state => 'active', :email => 'dm@x.com')
+      alice = User.new(:username => 'alice', :password => 'greatest', :email => 'dm4@x.com', :name => 'alice')
+      alice.save!
       User.authenticate('alice', 'greatest').should == alice
+      alice.delete()
   end
-  
+
 end
