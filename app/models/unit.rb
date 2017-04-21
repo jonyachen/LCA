90cdca6984eb420b205464c946328b96c6fc78bd
kerrorro <caroline.chen1101@gmail.com>
2017-04-21 08:10:30 +0000

    Fixed umbrella categories not factoring in units correctly

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,14 +2,18 @@ class Unit < ApplicationRecord
     def self.unit_conversion(id, units)
         # If the unit doesn't already match the activity's stored units, apply conversion
         activity_units = Activity.find(id).units
-        puts "UNITS:"
-        puts units
-        puts "ACTIVITY UNITS:"
-        puts activity_units
-        if (units != activity_units and activity_units.present?)
-            puts id
-            puts units
-            puts activity_units
+        
+        # For umbrella activities that don't have units, sample a default unit from its children
+        if !activity_units.present?
+            sample = Activity.where("parent_type = ? AND parent_id = ?", "Activity", id).first
+            while !sample.units.present? do
+                temp_id = sample.id
+                sample = Activity.where("parent_type = ? AND parent_id = ?", "Activity", temp_id).first
+            end
+            activity_units = sample.units
+        end
+        
+        if (units != activity_units)
             return Unit.conversion_factor_SI(units) * Unit.conversion_factor_from_SI(activity_units)
         else
             return 1.0
